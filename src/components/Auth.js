cd790ee0e08097eccf66b0c8e080add326cb3bfd
mazen <skywalker1951995@gmail.com>
2021-07-21 10:36:44 +0200

    simple auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,14 +1,21 @@
+import React, { useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import classes from './Auth.module.css'
-import { authActions } from '../store/auth-slice';
+import { authActions } from '../store/auth-slice'
 
 const Auth = () => {
 	const dispatch = useDispatch()
 	const isAuth = useSelector(state => state.auth.isAuth)
 
+	const emailRef = useRef()
+	const passwordRef = useRef()
+
 	const submitHandler = e => {
 		e.preventDefault()
-		dispatch(authActions.login())
+		const email = emailRef.current.value
+		const password = passwordRef.current.value
+		if (email.trim().length > 0 && password.trim().length > 0)
+			dispatch(authActions.login())
 	}
 
 	return (
@@ -18,11 +25,11 @@ const Auth = () => {
 					<form onSubmit={submitHandler}>
 						<div className={classes.control}>
 							<label htmlFor='email'>Email</label>
-							<input type='email' id='email' />
+							<input type='email' id='email' ref={emailRef} />
 						</div>
 						<div className={classes.control}>
 							<label htmlFor='password'>Password</label>
-							<input type='password' id='password' />
+							<input type='password' id='password' ref={passwordRef} />
 						</div>
 						<button>Login</button>
 					</form>
